1700c47262c0d8112ea18f058f751a45bcabea77
amrit607 <81628087+amrit607@users.noreply.github.com>
2022-06-03 13:52:42 +0530

    Add files via upload

---
+++ filter web app/main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+function preload(){
+mustach_face=loadImage("https://i.postimg.cc/m2r5ZXsd/m.png");
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("MODEL IS LOADED");
+}
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x="+noseX);
+        console.log("nose y="+ noseY);
+    }
+}
+function draw(){
+    image(video,0,0,300,300);
+ image(mustach_face,noseX-20,noseY,40,40)
+}
+function take_snapshot(){
+  save('myFilterimage.png');
+}
